7880bfb7285e897d80551be336932ba4cbf94fc9
jcapoduri <jcapoduri@gmail.com>
2018-01-12 18:57:35 -0300

    update core functions

--- libs/metru.core.pas
+++ libs/metru.core.pas
@@ -37,10 +37,13 @@ type
   procedure kickoff (var this : tMetruCore);
 
   { User functions }
-  function  login      (var this : tMetruCore, email, pass : string) : boolean;
+  function  login      (var this : tMetruCore; email, pass : string) : boolean;
   function  isLogedIn  (var this : tMetruCore) : boolean;
   function  loggedUser (var this : tMetruCore) : tUser;
   procedure logoff     (var this : tMetruCore);
+  function  createUser (var this : tMetruCore; tUser : user) : boolean;
+  function  updateUser (var this : tMetruCore; tUser : user) : boolean;
+  function  banUser    (var this : tMetruCore; tUser : user) : boolean;
 
   { category functions }
 
@@ -53,7 +56,7 @@ implementation
     lib.hash.open.loadHash(this.io.users, BASEPATH, CATEGORYFILE);
     lib.tree.lcrs.loadTree(this.io.categories, BASEPATH, CATEGORYFILE);
     lib.tree.trinary.loadTree(this.io.messages, BASEPATH, MESSAGEFILE);
-    lib.tree.close.loadHash(this.io.sells, BASEPATH, SELLSFILE);
+    lib.hash.close.loadHash(this.io.sells, BASEPATH, SELLSFILE);
   end;
 
   procedure kickoff (var this : tMetruCore);
@@ -61,7 +64,84 @@ implementation
     lib.hash.open.newEmptyHash(this.io.users, BASEPATH, CATEGORYFILE);
     lib.tree.lcrs.newEmptyTree(this.io.categories, BASEPATH, CATEGORYFILE);
     lib.tree.trinary.newEmptyTree(this.io.messages, BASEPATH, MESSAGEFILE);
-    lib.tree.close.newEmptyHash(this.io.sells, BASEPATH, SELLSFILE);
+    lib.hash.close.newEmptyHash(this.io.sells, BASEPATH, SELLSFILE);
   end;
 
+  function  login      (var this : tMetruCore; email, pass : string) : boolean;
+  var
+    found : boolean;
+    pos   : idxRange;
+    user  : tUser;
+  begin
+    found := this.hash.open.search(this.io.users, email, pos);
+    if (found) then
+      begin
+        user := this.hash.open.fetch(this.io.users, pos);
+        if (this.user.password = pass) then
+          begin
+            user.status := true;
+            lib.hash.open.insert(this.io.users, user);
+            this.user   := user;
+          end;
+        else
+          found := false;
+      end;
+    login := found;
+  end;
+
+  function  isLogedIn  (var this : tMetruCore) : boolean;
+  begin
+    isLogedIn := this.user.id > 0;
+  end;
+
+  function  loggedUser (var this : tMetruCore) : tUser;
+  begin
+    loggedUser := this.user;
+  end;
+
+  procedure logoff     (var this : tMetruCore);
+  var 
+    user : tUser
+    pos  : idxRange;
+  begin
+    this.user.status := false;
+    lib.hash.open.insert(this.io.users, this.user);
+    this.user.id := 0;
+  end;
+
+  function  createUser (var this : tMetruCore; tUser : user) : boolean;
+  var 
+    pos : tHashValue;
+    ok  : boolean;
+  begin
+    ok := not lib.hash.open.search(this.io.users, user.email, pos);
+    if ok then
+      lib.hash.open.insert(this.io.users, user);        
+    createUser := ok;
+  end;
+
+  function  updateUser (var this : tMetruCore; tUser : user) : boolean;
+  var 
+    pos : tHashValue;
+    ok  : boolean;
+  begin
+    ok := lib.hash.open.search(this.io.users, user.email, pos);
+    if ok then
+      lib.hash.open.insert(this.io.users, user);        
+    createUser := ok;
+  end;
+  
+  function  banUser    (var this : tMetruCore; tUser : user) : boolean;
+  var 
+    pos : tHashValue;
+    ok  : boolean;
+  begin
+    ok := lib.hash.open.search(this.io.users, user.email, pos);
+    if ok then
+      {TODO : search if has active publication}
+      lib.hash.open.remove(this.io.users, user);        
+      }
+    createUser := ok;
+  end;
+  
 end.
