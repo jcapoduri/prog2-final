72ef5d4afdb528a7ebdfd2606759f5720cdbb1bc
Jorge Capoduri <jorge.capoduri@avature.net>
2018-01-16 12:08:45 -0300

    add image library stub

---
+++ libs/lib.images.pas
@@ -0,0 +1,46 @@
+unit lib.images;
+
+interface
+
+uses
+  sysutils, md5;
+
+const
+  BASEPATH = 'data/images';
+
+type
+  tImageContainer = record
+                      path: string;
+                    end;
+
+  procedure setupContainer (var this : tImageContainer; path : string);
+  procedure storeImage     (var this : tImageContainer; pathToImage : string);
+  function  retrieveImage  (var this : tImageContainer; uniqId : string) : string;
+
+implementation
+  function _uniqId (image : string) : string;
+  var
+  begin
+    _uniqId := MD5Print(MD5String(image));
+  end;
+
+  procedure setupContainer (var this : tImageContainer; path : string);
+  begin
+    this.path := path;
+  end;
+
+  procedure storeImage     (var this : tImageContainer; pathToImage : string);
+  var
+    filename : string;
+  begin
+    filename := _uniqId(pathToImage);
+    copyfile(pathToImage, retrieveImage(this, filename));
+  end;
+  
+  function  retrieveImage  (var this : tImageContainer; uniqId : string) : string;
+  begin
+    retrieveImage := this.path + '/' + uniqId;
+  end;
+  
+
+end.
